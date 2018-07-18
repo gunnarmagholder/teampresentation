70ae27d8e73980b78e25ce043f8b2c4cf9a2cb60
Gunnar Magholder <gmagholder@gmail.com>
2018-07-18 11:19:11 +0200

    Adding dependency in the component itself

--- src/webparts/personPresenter/components/PersonPresenter.tsx
+++ src/webparts/personPresenter/components/PersonPresenter.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import styles from './PersonPresenter.module.scss';
 import { IPersonPresenterProps } from './IPersonPresenterProps';
 import { escape } from '@microsoft/sp-lodash-subset';
+import { PropertyFieldPeoplePicker, PrincipalType } from '@pnp/spfx-property-controls/lib/PropertyFieldPeoplePicker';
 
 export default class PersonPresenter extends React.Component<IPersonPresenterProps, {}> {
   public render(): React.ReactElement<IPersonPresenterProps> {

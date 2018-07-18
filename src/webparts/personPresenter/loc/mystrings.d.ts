553fdec9d59c9e3440654128e6de9df02f3ea766
Gunnar Magholder <gmagholder@gmail.com>
2018-07-18 10:21:37 +0200

    Scaffolding the webpart

---
+++ src/webparts/personPresenter/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IPersonPresenterWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'PersonPresenterWebPartStrings' {
+  const strings: IPersonPresenterWebPartStrings;
+  export = strings;
+}

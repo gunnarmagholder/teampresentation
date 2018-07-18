558024032438227b9e9aaa8067ef27218af90ad7
Gunnar Magholder <gmagholder@gmail.com>
2018-07-18 10:24:14 +0200

    Changing the look of the webpart canvas

--- src/webparts/personPresenter/components/PersonPresenter.tsx
+++ src/webparts/personPresenter/components/PersonPresenter.tsx
@@ -6,20 +6,7 @@ import { escape } from '@microsoft/sp-lodash-subset';
 export default class PersonPresenter extends React.Component<IPersonPresenterProps, {}> {
   public render(): React.ReactElement<IPersonPresenterProps> {
     return (
-      <div className={ styles.personPresenter }>
-        <div className={ styles.container }>
-          <div className={ styles.row }>
-            <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
-              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-              <p className={ styles.description }>{escape(this.props.description)}</p>
-              <a href="https://aka.ms/spfx" className={ styles.button }>
-                <span className={ styles.label }>Learn more</span>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
+      {this.props.description}
     );
   }
 }
